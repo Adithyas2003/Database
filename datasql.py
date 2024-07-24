d2998cabca28dd13ad1fc125812ce6a6548bc0b3
Adithyas2003 <adathyas49@gmail.com>
2024-07-24 11:31:56 +0530

    new file added

--- datasql.py
+++ datasql.py
@@ -31,15 +31,97 @@ except:
 
 
 
-b=con.execute("select * from student")
+# b=con.execute("select * from student")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in b:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+#user input:
+
+
+# name=str(input("name"))
+# d=con.execute("select * from student where name=?",(name,))
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in d:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+#Direct:
+
+# a=con.execute("select * from student where age=11")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in a:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+#user input of age:
+
+
+# age=int(input("age"))
+# d=con.execute("select * from student where age=?",(age,))
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in d:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+
+# d=con.execute("select * from student where age>=12")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in d:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+# d=con.execute("select * from student where mark<=25")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in d:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+#update:
+
+
+# d=con.execute("update student set name='anju'where name='anu'")
+# con.commit()
+# d=con.execute("select * from student")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in d:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+# update age:
+
+# a=con.execute("update student set age=23 where age=22")
+# con.commit()
+# a=con.execute("select * from student")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in a:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+
+# a=con.execute("update student set age=23 where age=22")
+# con.commit()
+# a=con.execute("select * from student")
+# print("{:<10}{:<16}".format('name:','age:',))
+# for i in a:
+#     print("{:<10}{:<16}".format(i[0],i[1]))
+
+
+#delete:
+
+# a=con.execute("delete from student where  name='anju'")
+# con.commit()
+# a=con.execute("select * from student")
+# print("{:<10}{:<16}{:<10}".format('name','age','mark'))
+# for i in a:
+#     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
+
+name=str(input("name"))
+a=con.execute("delete from student where  name='appu'")
+con.commit()
+a=con.execute("select * from student where name=?",(name,))
 print("{:<10}{:<16}{:<10}".format('name','age','mark'))
-for i in b:
+for i in a:
     print("{:<10}{:<16}{:<10}".format(i[0],i[1],i[2]))
-
-
-
-
-
 
 
 
---
+++ employee.py
@@ -0,0 +1,26 @@
+import sqlite3
+con=sqlite3.connect("employee.db")
+try:
+    con.execute("create table staff(name text,position text,age int, salary real) ")
+except:
+       pass
+while True:
+    print("1.add")
+    print("2.update")
+    print("3.display")
+    print("4.search")
+    print("5.delete")
+    ch=input("enter your choice")
+    if ch=='1':
+         name=input("enter name:")
+         position=input("enter position:")
+         age=int(input("enter the age:"))
+         salary=float(input("enter the salary:"))
+         con.execute("insert into staff(name,position,age,salary)values('kiran','HR',28,30000)")
+         con.commit()
+         print("Employee Added Successfully")
+    elif ch=='2':
+         name=input("enter the name:")
+
+
+      
